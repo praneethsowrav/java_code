9d20459a2cc0a513914bfd960bb76fcd4dcf6d72
praneethsowrav <praneethsowrav07@gmail.com>
2025-01-29 09:44:33 +0530

    Create product.java

---
+++ product.java
@@ -0,0 +1,10 @@
+class product
+  {
+    public static void main(string args[])
+    {
+      int a=11;
+      int b=77;
+      int product=a*b;
+      system,out,println("product of the two numbers is"+product);
+     }
+   }
